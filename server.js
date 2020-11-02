85e4be65c5f26ec79ad73bc571a0380abb39e466
akshaymulay <53270331+akshaymulay@users.noreply.github.com>
2020-11-02 17:49:08 +0530

    'auth_system'

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose= require('mongoose')
 const session=require('express-session')
 const flash=require('express-flash')
 const MongoDbStore=require('connect-mongo')(session)
+const passport=require('passport')
 
 //db connection
 const url='mongodb://localhost:27017/pizza';
@@ -19,6 +20,7 @@ connection.once('open',()=>{
 }).catch(err=>{
 	console.log('connection failed...');
 });
+
 
 //session store
 let mongoStore=new MongoDbStore({
@@ -34,15 +36,24 @@ app.use(session({
 	cookie:{maxAge:1000*60*60*24} //24hrs
 }))
 
+//passport config
+const passportInit=require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 app.use(flash())
 //Assets
 app.use(express.static('public'))
 
+app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 //global middleware
 app.use((req,res,next)=>{
 	res.locals.session=req.session
+	res.locals.user=req.user
 	next()
 
 })
