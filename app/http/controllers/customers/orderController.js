82f25e7c17c046ebe5dff603f6abe9f7a4bb262e
akshaymulay <53270331+akshaymulay@users.noreply.github.com>
2020-11-08 18:40:01 +0530

    Update orderController.js

--- app/http/controllers/customers/orderController.js
+++ app/http/controllers/customers/orderController.js
@@ -11,7 +11,7 @@ function orderController(){
 				}
 				const order=new Order({
 					customerId:req.user._id,
-					items:req.session.cart.items,http://localhost:3000/img/hero-pizza.png
+					items:req.session.cart.items,
 					phone,
 					address
 				})
